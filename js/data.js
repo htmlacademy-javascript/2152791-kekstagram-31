560578682e5ee11efce3e6419360561a30d02b77
T-800 <euronymous.ts@yandex.ru>
2024-02-29 11:30:53 +0300

    Bug fix#2

---
+++ js/data.js
@@ -0,0 +1,26 @@
+import {getRandomNumber} from './utils.js';
+import {getComments, idArray, urlArray, photoCount} from './generators.js';
+
+const createPhoto = () => {
+  const photoDescription = [
+    'Волки рядятся в овечью шкуру ради своих шкурных интересов',
+    'Запомните волчья ягода не из волков',
+    'Волк слабее льва и тигра, но в цирке не выступает',
+    'Я словно волк — всегда осторожный и в меру голодный',
+    'Лучше один день прожить волком, чем 100 лет шакалом',
+    'Бойся гнева терпеливого волка',
+    'Волк чужого не ищет, волк довольствуется своим'
+  ];
+
+  return {
+    id: idArray.shift(),
+    url: `photos/${urlArray.shift()}.jpg`,
+    description: photoDescription[getRandomNumber(0, 6)],
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(0, 30)}, getComments)
+  };
+};
+
+const photoArray = () => Array.from({length: photoCount}, createPhoto);
+
+export {photoArray};
---
+++ js/generators.js
@@ -0,0 +1,51 @@
+import {getRandomNumber, shuffleArray} from './utils.js';
+
+const idArray = [];
+const urlArray = [];
+const commentsIdArray = [];
+const photoCount = 25;
+const idCount = 30;
+
+const generateRandomArrays = () => {
+  for (let i = 1; i <= photoCount; i++) {
+    idArray.push(i);
+    urlArray.push(i);
+  }
+
+  for (let i = 1; i <= idCount; i++) {
+    commentsIdArray.push(i);
+  }
+
+  shuffleArray(idArray);
+  shuffleArray(urlArray);
+  shuffleArray(commentsIdArray);
+};
+generateRandomArrays();
+
+const getComments = () => {
+  const commentMessage = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const names = [
+    'Тимофей',
+    'Майя',
+    'Иван',
+    'Рита',
+    'Антонина',
+    'Анна',
+  ];
+
+  return {
+    id: commentsIdArray.shift(),
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: commentMessage[getRandomNumber(0, 5)],
+    name: names[getRandomNumber(0, 5)]
+  };
+};
+
+export {getComments, idArray, urlArray, photoCount};
